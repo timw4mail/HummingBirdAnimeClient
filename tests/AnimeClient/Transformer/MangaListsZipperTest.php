60109b3531d9808a3af21ff27260f51342b17c37
Timothy J. Warren <tim@timshomepage.net>
2015-09-18 13:06:22 -0400

    Fix failing tests for PHP < 5.6

--- tests/AnimeClient/Transformer/MangaListsZipperTest.php
+++ tests/AnimeClient/Transformer/MangaListsZipperTest.php
@@ -3,22 +3,25 @@
 use Aviat\AnimeClient\Transformer\Hummingbird\MangaListsZipper;
 
 class MangaListsZipperTest extends AnimeClient_TestCase {
-	
-	protected $start_file = __DIR__ . '/../../test_data/manga_list/manga.json';
-	protected $res_file = __DIR__ . '/../../test_data/manga_list/manga-zippered.json';
+
+	protected $start_file = '';
+	protected $res_file = '';
 
 	public function setUp()
 	{
+		$this->start_file = __DIR__ . '/../../test_data/manga_list/manga.json';
+		$this->res_file = __DIR__ . '/../../test_data/manga_list/manga-zippered.json';
+
 		$json = json_decode(file_get_contents($this->start_file), TRUE);
 		$this->mangaListsZipper = new MangaListsZipper($json);
 	}
-	
+
 	public function testTransform()
 	{
 		$zippered_json = json_decode(file_get_contents($this->res_file), TRUE);
 		$transformed = $this->mangaListsZipper->transform();
-		
+
 		$this->assertEquals($zippered_json, $transformed);
 	}
-	
+
 }
