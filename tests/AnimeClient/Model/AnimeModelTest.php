b68cbe5a2648d7fda86925ddf46eb6bc2e374adc
Timothy J. Warren <tim@timshomepage.net>
2015-10-01 16:07:40 -0400

    fix test

--- tests/AnimeClient/Model/AnimeModelTest.php
+++ tests/AnimeClient/Model/AnimeModelTest.php
@@ -1,11 +1,18 @@
 <?php
 
 use Aviat\Ion\Friend;
+use Aviat\Ion\Di\ContainerInterface;
 use Aviat\AnimeClient\Model\Anime as AnimeModel;
 
 class AnimeMock extends AnimeModel {
 
-	protected $transformed_data_file = __DIR__ . "/../../test_data/anime_list/anime-completed-transformed.json";
+	protected $transformed_data_file;
+
+	public function __construct(ContainerInterface $container)
+	{
+		parent::__construct($container);
+		$this->transformed_data_file = __DIR__ . "/../../test_data/anime_list/anime-completed-transformed.json";
+	}
 
 	protected function _get_list_from_api($status="all")
 	{
