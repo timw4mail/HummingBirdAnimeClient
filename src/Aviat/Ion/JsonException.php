fa4940f22d730a1c24c71be3eb22ff8b7840671c
Timothy J Warren <tim@timshomepage.net>
2016-01-06 17:08:44 -0500

    Add missing classes

---
+++ src/Aviat/Ion/JsonException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Ion
+ *
+ * Building blocks for web development
+ *
+ * @package     Ion
+ * @author      Timothy J. Warren
+ * @copyright   Copyright (c) 2015 - 2016
+ * @license     MIT
+ */
+
+namespace Aviat\Ion;
+
+class JsonException extends \InvalidArgumentException {
+
+}
+// End of JsonException.php
