3d19f930016350d6a12d4d3c50434b0a86919c4c
Timothy J Warren <tim@timshomepage.net>
2016-04-07 12:34:57 -0400

    Add tests for SQL based api cache

---
+++ tests/Ion/Cache/Driver/SQLDriverTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require('CacheDriverBase.php');
+
+use Aviat\Ion\Friend;
+use Aviat\Ion\Cache\Driver\SQLDriver;
+
+class CacheSQLDriverTest extends AnimeClient_TestCase {
+	use CacheDriverBase;
+	
+	protected $driver;
+	
+	public function setUp()
+	{
+		parent::setUp();
+		$this->driver = new SQLDriver($this->container);
+		$friend = new Friend($this->driver);
+		$friend->db->query('CREATE TABLE IF NOT EXISTS "cache" ("key" TEXT NULL, "value" TEXT NULL, PRIMARY KEY ("key"))');
+	}
+}
