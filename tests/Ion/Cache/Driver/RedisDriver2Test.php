672552a1e8dc6dcd558fef697007ef34034f2cd9
Timothy J. Warren <tim@timshomepage.net>
2016-07-18 12:59:34 -0400

    Make tests skip redis integration if the extension is not installed

--- tests/Ion/Cache/Driver/RedisDriver2Test.php
+++ tests/Ion/Cache/Driver/RedisDriver2Test.php
@@ -15,22 +15,33 @@ class CacheRedisDriverTestTwo extends AnimeClient_TestCase {
 	{
 		parent::setUp();
 
-		// Setup config with port and password
-		$container = new Container();
-		$container->set('config', new Config([
-			'redis' => [
-				'host' => 'localhost',
-				'port' => 6379,
-				'password' => '',
-				'database' => 13,
-			]
-		]));
-		$this->driver = new RedisDriver($container);
+		if ( ! class_exists('Redis'))
+		{
+			$this->markTestSkipped('Redis extension not installed');
+		}
+		else
+		{
+			// Setup config with port and password
+			$container = new Container();
+			$container->set('config', new Config([
+				'redis' => [
+					'host' => 'localhost',
+					'port' => 6379,
+					'password' => '',
+					'database' => 13,
+				]
+			]));
+			$this->driver = new RedisDriver($container);
+		}
 	}
 
 	public function tearDown()
 	{
 		parent::tearDown();
-		$this->driver->__destruct();
+
+		if ( ! is_null($this->driver))
+		{
+			$this->driver->__destruct();
+		}
 	}
 }
