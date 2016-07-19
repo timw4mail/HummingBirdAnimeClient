ca62a411e16197b914ee93f3bc1076aaa0a4872c
Timothy J. Warren <tim@timshomepage.net>
2016-07-19 10:25:54 -0400

    Attempt 2 to install gd in gitlab ci tests

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -7,7 +7,7 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git unzip libfreetype6 libjpeg62-turbo libmcrypt4 libpng12-0 sendmail -yqq
+apt-get install git unzip libfreetype6 libjpeg62-turbo libmcrypt4 libpng12-0 libfreetype6-dev libjpeg-dev libmcrypt-dev libpng12-dev zlib1g-dev -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
