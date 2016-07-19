d54d180090b45629950b4b532444bbf7c2f9d034
Timothy J Warren <tim@timshomepage.net>
2016-07-19 10:17:53 -0400

    Fix failing test by installing gd in gitlab ci test

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -7,8 +7,15 @@ set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
 apt-get update -yqq
-apt-get install git unzip -yqq
+apt-get install git unzip libfreetype6 libjpeg62-turbo libmcrypt4 libpng12-0 sendmail -yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
+
+# Install gd
+docker-php-ext-configure gd --enable-gd-native-ttf --with-jpeg-dir=/usr/lib/x86_64-linux-gnu --with-png-dir=/usr/lib/x86_64-linux-gnu --with-freetype-dir=/usr/lib/x86_64-linux-gnu
+docker-php-ext-install gd
+docker-php-ext-install mcrypt
+docker-php-ext-install mbstring
+docker-php-ext-install zip
