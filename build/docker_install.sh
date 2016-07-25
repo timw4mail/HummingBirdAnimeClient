fec9de0e3db233f1a5367451a1d01c651b7640e9
Timothy J Warren <tim@timshomepage.net>
2016-07-25 11:58:43 -0400

    Add redis config file for gitlab ci tests

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -3,6 +3,9 @@
 # We need to install dependencies only for Docker
 [[ ! -e /.dockerenv ]] && [[ ! -e /.dockerinit ]] && exit 0
 
+# Where am I?
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
@@ -12,6 +15,10 @@ apt-get install git unzip libfreetype6 libjpeg62-turbo libmcrypt4 libpng12-0 lib
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
+
+# Move redis test config file into place
+mv "$DIR/test/redis.toml" "$DIR/app/config/redis.toml"
+
 
 # Install gd
 docker-php-ext-configure gd --enable-gd-native-ttf --with-jpeg-dir=/usr/lib/x86_64-linux-gnu --with-png-dir=/usr/lib/x86_64-linux-gnu --with-freetype-dir=/usr/lib/x86_64-linux-gnu
