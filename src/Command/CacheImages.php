230c80459e7a1bcb95e30ebf92029af9f0886771
Timothy J. Warren <tim@timshomepage.net>
2016-08-30 10:01:18 -0400

    Update header comments

--- src/Command/CacheImages.php
+++ src/Command/CacheImages.php
@@ -4,11 +4,14 @@
  *
  * An API client for Hummingbird to manage anime and manga watch lists
  *
+ * PHP version 5.6
+ *
  * @package     HummingbirdAnimeClient
- * @author      Timothy J. Warren
- * @copyright   Copyright (c) 2015 - 2016
+ * @author      Timothy J. Warren <tim@timshomepage.net>
+ * @copyright   2015 - 2016  Timothy J. Warren
+ * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version     3.1
  * @link        https://github.com/timw4mail/HummingBirdAnimeClient
- * @license     MIT
  */
 
 namespace Aviat\AnimeClient\Command;
@@ -72,7 +75,7 @@ class CacheImages extends BaseCommand {
 	 * @return void
 	 * @throws \ConsoleKit\ConsoleException
 	 */
-	public function execute(array $args, array $options = array())
+	public function execute(array $args, array $options = [])
 	{
 		$this->setContainer($this->setupContainer());
 		$this->util = new Util($this->container);
