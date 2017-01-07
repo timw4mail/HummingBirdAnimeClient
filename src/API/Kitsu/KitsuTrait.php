187812576c644852ff2123e15826a73566eea242
Timothy J Warren <tim@timshomepage.net>
2017-01-06 23:34:56 -0500

    Update header comments

--- src/API/Kitsu/KitsuTrait.php
+++ src/API/Kitsu/KitsuTrait.php
@@ -6,12 +6,12 @@
  *
  * PHP version 7
  *
- * @package	 AnimeListClient
- * @author	  Timothy J. Warren <tim@timshomepage.net>
+ * @package     AnimeListClient
+ * @author      Timothy J. Warren <tim@timshomepage.net>
  * @copyright   2015 - 2016  Timothy J. Warren
- * @license	 http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version	 4.0
- * @link		https://github.com/timw4mail/HummingBirdAnimeClient
+ * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version     4.0
+ * @link        https://github.com/timw4mail/HummingBirdAnimeClient
  */
 
 namespace Aviat\AnimeClient\API\Kitsu;
