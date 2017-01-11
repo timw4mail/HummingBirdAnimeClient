c76bb4d32ad2395964d15bc81bb3d9279a032e52
Timothy J. Warren <tim@timshomepage.net>
2017-01-11 10:30:53 -0500

    Update header comments

--- src/API/Kitsu/KitsuModel.php
+++ src/API/Kitsu/KitsuModel.php
@@ -8,13 +8,11 @@
  *
  * @package     AnimeListClient
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2016  Timothy J. Warren
+ * @copyright   2015 - 2017  Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version     4.0
  * @link        https://github.com/timw4mail/HummingBirdAnimeClient
- */
-
-namespace Aviat\AnimeClient\API\Kitsu;
+ */namespace Aviat\AnimeClient\API\Kitsu;
 
 use Aviat\AnimeClient\API\Kitsu as K;
 use Aviat\AnimeClient\API\Kitsu\Transformer\{
