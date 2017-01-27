a80a860f8d21cb72549efcb7652a5f0e64b5cdd5
Timothy J. Warren <tim@timshomepage.net>
2017-01-27 12:09:05 -0500

    Attempt simpler php setup, with xdebug

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -13,27 +13,14 @@ apt-get update -yqq
 apt-get install \
 	git \
 	unzip \
-	libfreetype6 \
-	libjpeg62-turbo \
-	libmcrypt4 \
-	libpng12-0 \
-	libfreetype6-dev \
-	libjpeg-dev \
-	libmcrypt-dev \
-	libpng12-dev \
-	libxslt1-dev \
-	libxslt1.1 \
-	zlib1g-dev \
 	-yqq
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
-# Install gd
-docker-php-ext-configure gd --enable-gd-native-ttf --with-jpeg-dir=/usr/lib/x86_64-linux-gnu --with-png-dir=/usr/lib/x86_64-linux-gnu --with-freetype-dir=/usr/lib/x86_64-linux-gnu
-docker-php-ext-install gd
-docker-php-ext-install mcrypt
+# Install extensions
+pecl install xdebug
+echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
 docker-php-ext-install xsl
 docker-php-ext-install zip
-docker-php-ext-install xdebug
