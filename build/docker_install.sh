90f1b39db58186f5fa1703e9e5f9ff9fd70d4c79
Timothy J. Warren <tim@timshomepage.net>
2017-02-15 09:42:20 -0500

    Try to use xdebug another way

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -11,13 +11,15 @@ apk upgrade --update && apk add \
 	git \
 	libxslt-dev \
 	zlib-dev
+	
+apk add php7-xdebug --repository http://dl-3.alpinelinux.org/alpine/edge/testing/
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
 chmod +x /usr/local/bin/phpunit
 
 # Install extensions
-pecl install xdebug
-echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)" > /usr/local/etc/php/conf.d/xdebug.ini
-docker-php-ext-install xsl
-docker-php-ext-install zip
+#pecl install xdebug
+echo "zend_extension=/usr/lib/php7/modules/xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
+#docker-php-ext-install xsl
+#docker-php-ext-install zip
