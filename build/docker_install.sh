acbae86a6b8aa54f243b1ac4a825418a193360ca
Timothy J. Warren <tim@timshomepage.net>
2017-02-15 10:12:18 -0500

    Slim build config

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -12,12 +12,7 @@ apk upgrade --update && apk add --no-cache \
 	autoconf \
 	curl \
 	git \
-	libxslt-dev \
 	zlib-dev
-	
-# Install phpunit, the tool that we will use for testing
-#curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-#chmod +x /usr/local/bin/phpunit
 
 # Install extensions
 # Install xdebug for coverage report
@@ -25,4 +20,4 @@ docker-php-source extract
 pecl install xdebug
 docker-php-ext-enable xdebug
 docker-php-source delete
-docker-php-ext-install zip
+# docker-php-ext-install zip
