5e1600d35d44ab25b2eea58150b56010f6b00075
Timothy J Warren <tim@timshomepage.net>
2017-02-15 09:47:52 -0500

    xdebug try again

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -12,7 +12,12 @@ apk upgrade --update && apk add \
 	libxslt-dev \
 	zlib-dev
 	
-apk add php7-xdebug --repository http://dl-3.alpinelinux.org/alpine/edge/testing/
+pecl install xdebug
+
+echo "zend_extension=$(find /usr/local/lib/php/extensions/ -name xdebug.so)\n" >> /usr/local/etc/php/conf.d/xdebug.ini
+#echo "xdebug.remote_enable=on\n" >> /usr/local/etc/php/conf.d/xdebug.ini
+#echo "xdebug.remote_autostart=off\n" >> /usr/local/etc/php/conf.d/xdebug.ini
+#echo "xdebug.remote_port=9000\n" >> /usr/local/etc/php/conf.d/xdebug.ini
 
 # Install phpunit, the tool that we will use for testing
 curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
@@ -20,6 +25,6 @@ chmod +x /usr/local/bin/phpunit
 
 # Install extensions
 #pecl install xdebug
-echo "zend_extension=/usr/lib/php7/modules/xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
+#echo "zend_extension=/usr/lib/php7/modules/xdebug.so" > /usr/local/etc/php/conf.d/xdebug.ini
 #docker-php-ext-install xsl
 #docker-php-ext-install zip
