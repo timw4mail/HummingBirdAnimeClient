6ed755e2520f9057e4a65b0263777f36feda82ae
Timothy J. Warren <tim@timshomepage.net>
2017-02-16 11:47:54 -0500

    JS style fixes

--- public/js/anime_collection.js
+++ public/js/anime_collection.js
@@ -4,7 +4,7 @@
 
 	const search = (tempHtml, query) => {
 		_.$('.cssload-loader')[0].removeAttribute('hidden');
-		_.get(_.url('/collection/search'), {'query':query}, (searchResults, status) => {
+		_.get(_.url('/collection/search'), {query}, (searchResults, status) => {
 			searchResults = JSON.parse(searchResults);
 			_.$('.cssload-loader')[0].setAttribute('hidden', 'hidden');
 
@@ -18,7 +18,7 @@
 		});
 	};
 
-	_.get('/public/templates/anime-ajax-search-results.html', tempHtml => {
+	_.get('/public/templates/anime-ajax-search-results.html', (tempHtml) => {
 		_.on('#search', 'keyup', _.throttle(250, function(e) {
 			let query = encodeURIComponent(this.value);
 			if (query === '') {
