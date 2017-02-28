92a1b147fc3633a04ea447df49091a1dfe99a177
Timothy J Warren <tim@timshomepage.net>
2017-02-28 13:17:06 -0500

    Simplify gitlab ci setup

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -6,20 +6,7 @@
 set -xe
 
 # Install git (the php image doesn't have it) which is required by composer
+echo -e 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories
 apk upgrade --update && apk add --no-cache \
-	g++ \
-	make \
-	autoconf \
 	curl \
 	git
-	
-# Install phpunit, the tool that we will use for testing
-curl -Lo /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-chmod +x /usr/local/bin/phpunit
-
-# Install extensions
-# Install xdebug for coverage report
-docker-php-source extract
-pecl install xdebug
-docker-php-ext-enable xdebug
-docker-php-source delete
