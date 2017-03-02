88f6a3fc4ff79e881870c0d2f3ee0d581fa36356
Timothy J Warren <tim@timshomepage.net>
2017-03-01 22:07:51 -0500

    Rework the rest of the mappings

---
+++ src/API/Enum/AnimeWatchingStatus/Title.php
@@ -0,0 +1,31 @@
+<?php declare(strict_types=1);
+/**
+ * Hummingbird Anime List Client
+ *
+ * An API client for Kitsu and MyAnimeList to manage anime and manga watch lists
+ *
+ * PHP version 7
+ *
+ * @package     HummingbirdAnimeClient
+ * @author      Timothy J. Warren <tim@timshomepage.net>
+ * @copyright   2015 - 2017  Timothy J. Warren
+ * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version     4.0
+ * @link        https://github.com/timw4mail/HummingBirdAnimeClient
+ */
+
+namespace Aviat\AnimeClient\API\Enum\AnimeWatchingStatus;
+
+use Aviat\Ion\Enum as Enum;
+
+/**
+ * Possible values for current watching status of anime
+ */
+class Title extends Enum {
+	const ALL = 'All';
+	const WATCHING = 'Currently Watching';
+	const PLAN_TO_WATCH = 'Plan to Watch';
+	const DROPPED = 'Dropped';
+	const ON_HOLD = 'On Hold';
+	const COMPLETED = 'Completed';
+}
