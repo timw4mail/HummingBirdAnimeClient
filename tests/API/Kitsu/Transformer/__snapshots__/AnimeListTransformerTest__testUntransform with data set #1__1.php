472be3c4ed0e5a2541a81897d9248c9edba0fcb1
Timothy J. Warren <tim@timshomepage.net>
2017-04-06 11:59:53 -0400

    Use snapshots library with tests to simplify testcases

---
+++ tests/API/Kitsu/Transformer/__snapshots__/AnimeListTransformerTest__testUntransform with data set #1__1.php
@@ -0,0 +1,14 @@
+<?php return array (
+  'id' => 14047981,
+  'mal_id' => '12345',
+  'data' => 
+  array (
+    'status' => 'current',
+    'reconsuming' => true,
+    'reconsumeCount' => 0,
+    'notes' => 'Very formulaic.',
+    'progress' => 38,
+    'private' => true,
+    'rating' => 4,
+  ),
+);
