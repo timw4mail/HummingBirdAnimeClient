b444648a3d5299e41aed762063808f43d6210947
Timothy J Warren <tim@timshomepage.net>
2017-04-06 11:59:53 -0400

    Use snapshots library with tests to simplify testcases

---
+++ tests/API/Kitsu/Transformer/__snapshots__/AnimeListTransformerTest__testTransform__1.php
@@ -0,0 +1,45 @@
+<?php return array (
+  'id' => '15839442',
+  'mal_id' => '33206',
+  'episodes' => 
+  array (
+    'watched' => 0,
+    'total' => '-',
+    'length' => NULL,
+  ),
+  'airing' => 
+  array (
+    'status' => 'Currently Airing',
+    'started' => '2017-01-12',
+    'ended' => NULL,
+  ),
+  'anime' => 
+  array (
+    'age_rating' => NULL,
+    'title' => 'Kobayashi-san Chi no Maid Dragon',
+    'titles' => 
+    array (
+      0 => 'Kobayashi-san Chi no Maid Dragon',
+      1 => 'Miss Kobayashi\'s Dragon Maid',
+      2 => '小林さんちのメイドラゴン',
+    ),
+    'slug' => 'kobayashi-san-chi-no-maid-dragon',
+    'type' => 'TV',
+    'image' => 'https://media.kitsu.io/anime/poster_images/12243/small.jpg?1481144116',
+    'genres' => 
+    array (
+      0 => 'Comedy',
+      1 => 'Fantasy',
+      2 => 'Slice of Life',
+    ),
+    'streaming_links' => 
+    array (
+    ),
+  ),
+  'watching_status' => 'current',
+  'notes' => NULL,
+  'rewatching' => false,
+  'rewatched' => 0,
+  'user_rating' => '-',
+  'private' => false,
+);
