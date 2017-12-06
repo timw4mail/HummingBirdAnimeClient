7703187cd9005d827afd3ccdd826fd63d7080fd3
Timothy J Warren <tim@timshomepage.net>
2017-12-06 11:07:23 -0500

    Attempt to fix gitlab ci build

--- build/docker_install.sh
+++ build/docker_install.sh
@@ -9,4 +9,5 @@ set -xe
 echo -e 'http://dl-cdn.alpinelinux.org/alpine/edge/main\nhttp://dl-cdn.alpinelinux.org/alpine/edge/community\nhttp://dl-cdn.alpinelinux.org/alpine/edge/testing' > /etc/apk/repositories
 apk upgrade --update && apk add --no-cache \
 	curl \
-	git
+	git \
+	ipv6
