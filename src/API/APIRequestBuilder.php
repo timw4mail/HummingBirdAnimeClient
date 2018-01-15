7b9adbf52e8b34a4008eed17d3ea821238f64b90
Timothy J. Warren <tim@timshomepage.net>
2018-01-15 14:43:15 -0500

    Update copyright year

--- src/API/APIRequestBuilder.php
+++ src/API/APIRequestBuilder.php
@@ -8,7 +8,7 @@
  *
  * @package     HummingbirdAnimeClient
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2017  Timothy J. Warren
+ * @copyright   2015 - 2018  Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
  * @version     4.0
  * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
