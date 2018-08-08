e01a96b8fb0c7c2738c88e02d5bc9c4e77d2a306
Timothy J. Warren <tim@timshomepage.net>
2018-08-08 17:04:35 -0400

    First go at tabs for collection

--- app/views/manga/edit.php
+++ app/views/manga/edit.php
@@ -47,7 +47,7 @@
 				<tr>
 					<td><label for="volumes_read">Volumes Read</label></td>
 					<td>
-						<input type="text" disabled="disabled" min="0" name="volumes_read" id="volumes_read" value="-" /> / <?= $item['volumes']['total'] ?>
+						<?php /*<input type="number" disabled="disabled" min="0" name="volumes_read" id="volumes_read" value="" /> */ ?> - / <?= $item['volumes']['total'] ?>
 					</td>
 				</tr>
 				<tr>
