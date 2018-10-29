3842df13dbf910c208a24b8c4665503c6563507a
Timothy J. Warren <tim@timshomepage.net>
2018-10-29 15:48:54 -0400

    Small code consistency update

--- tests/API/Kitsu/Transformer/__snapshots__/MangaTransformerTest__testTransform__1.php
+++ tests/API/Kitsu/Transformer/__snapshots__/MangaTransformerTest__testTransform__1.php
@@ -5,6 +5,68 @@
   array (
   ),
    'id' => '20286',
+   'included' => 
+  array (
+    'genres' => 
+    array (
+      3 => 
+      array (
+        'attributes' => 
+        array (
+          'name' => 'Comedy',
+          'slug' => 'comedy',
+          'description' => NULL,
+        ),
+      ),
+      24 => 
+      array (
+        'attributes' => 
+        array (
+          'name' => 'School',
+          'slug' => 'school',
+          'description' => NULL,
+        ),
+      ),
+      16 => 
+      array (
+        'attributes' => 
+        array (
+          'name' => 'Slice of Life',
+          'slug' => 'slice-of-life',
+          'description' => '',
+        ),
+      ),
+      14 => 
+      array (
+        'attributes' => 
+        array (
+          'name' => 'Romance',
+          'slug' => 'romance',
+          'description' => '',
+        ),
+      ),
+      18 => 
+      array (
+        'attributes' => 
+        array (
+          'name' => 'Thriller',
+          'slug' => 'thriller',
+          'description' => NULL,
+        ),
+      ),
+    ),
+    'mappings' => 
+    array (
+      48014 => 
+      array (
+        'attributes' => 
+        array (
+          'externalSite' => 'myanimelist/manga',
+          'externalId' => '26769',
+        ),
+      ),
+    ),
+  ),
    'manga_type' => 'manga',
    'synopsis' => 'Usa, a high-school student aspiring to begin a bachelor lifestyle, moves into a new apartment only to discover that he not only shares a room with a perverted roommate that has an obsession for underaged girls, but also that another girl, Ritsu, a love-at-first-sight, is living in the same building as well!
 (Source: Kirei Cake)',
