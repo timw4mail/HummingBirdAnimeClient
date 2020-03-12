986ff6de0bf80c8dcc62422543c65657bece9227
Timothy J. Warren <tim@timshomepage.net>
2020-03-12 11:45:11 -0400

    Update header comments again

--- src/Ion/Transformer/AbstractTransformer.php
+++ src/Ion/Transformer/AbstractTransformer.php
@@ -1,17 +1,17 @@
 <?php declare(strict_types=1);
 /**
- * Ion
+ * Hummingbird Anime List Client
  *
- * Building blocks for web development
+ * An API client for Kitsu to manage anime and manga watch lists
  *
- * PHP version 7.2
+ * PHP version 7.3
  *
- * @package     Ion
+ * @package     HummingbirdAnimeClient
  * @author      Timothy J. Warren <tim@timshomepage.net>
- * @copyright   2015 - 2019 Timothy J. Warren
+ * @copyright   2015 - 2020  Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     3.0.0
- * @link        https://git.timshomepage.net/aviat/ion
+ * @version     4.2
+ * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
  */
 
 namespace Aviat\Ion\Transformer;
