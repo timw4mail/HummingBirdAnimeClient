43f07dac6c5325aa0d8da1d9025b1d0e471970b8
Timothy J. Warren <tim@timshomepage.net>
2020-05-06 13:16:40 -0400

    Set up Event-based handling for a few things

---
+++ src/AnimeClient/Enum/EventType.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * Hummingbird Anime List Client
+ *
+ * An API client for Kitsu to manage anime and manga watch lists
+ *
+ * PHP version 7.4
+ *
+ * @package     HummingbirdAnimeClient
+ * @author      Timothy J. Warren <tim@timshomepage.net>
+ * @copyright   2015 - 2020  Timothy J. Warren
+ * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version     5
+ * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
+ */
+
+namespace Aviat\AnimeClient\Enum;
+
+use Aviat\Ion\Enum as BaseEnum;
+
+final class EventType extends BaseEnum {
+	public const CLEAR_CACHE = '::clear-cache::';
+	public const RESET_CACHE_KEY = '::reset-cache-key::';
+	public const UNAUTHORIZED = '::unauthorized::';
+}
