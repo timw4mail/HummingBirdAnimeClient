c4fb095eba5e3bc715fc4f4ae4a0cd4491a0285c
Timothy J Warren <tim@timshomepage.net>
2020-05-08 19:16:04 -0400

    Fix settings page subforms (so all the fields show for the cache)

--- app/views/settings/_form.php
+++ app/views/settings/_form.php
@@ -6,19 +6,19 @@
 ?>
 
 <?php foreach ($fields as $name => $field): ?>
-	<?php $fieldname = ($section === 'config' || $nestedPrefix !== 'config') ? "{$nestedPrefix}[{$name}]" : "{$nestedPrefix}[{$section}][{$name}]"; ?>
+	<?php
+		$fieldName = ($section === 'config' || $nestedPrefix !== 'config')
+				? "{$nestedPrefix}[{$name}]"
+				: "{$nestedPrefix}[{$section}][{$name}]";
+	?>
 	<?php if ($field['type'] === 'subfield'): ?>
 		<section>
 			<h4><?= $field['title'] ?></h4>
-			<?php include_once '_form.php'; ?>
+			<?php include '_subfield.php'; ?>
 		</section>
 	<?php elseif ( ! empty($field['display'])): ?>
-		<article>
-			<label for="<?= $fieldname ?>"><?= $field['title'] ?></label><br />
-			<small><?= $field['description'] ?></small><br />
-			<?= $helper->field($fieldname, $field); ?>
-		</article>
+		<?php include '_field.php' ?>
 	<?php else: ?>
-		<?php $hiddenFields[] = $helper->field($fieldname, $field); ?>
+		<?php $hiddenFields[] = $helper->field($fieldName, $field); ?>
 	<?php endif ?>
 <?php endforeach ?>
