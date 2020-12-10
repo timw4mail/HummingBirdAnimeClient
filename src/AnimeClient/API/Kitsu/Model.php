3c47570cceafe476bf31d7983f2190651177facc
Timothy J. Warren <tim@timshomepage.net>
2020-12-10 17:06:50 -0500

    Bump version in header comments

--- src/AnimeClient/API/Kitsu/Model.php
+++ src/AnimeClient/API/Kitsu/Model.php
@@ -4,13 +4,13 @@
  *
  * An API client for Kitsu to manage anime and manga watch lists
  *
- * PHP version 7.4
+ * PHP version 7.4+
  *
  * @package     HummingbirdAnimeClient
  * @author      Timothy J. Warren <tim@timshomepage.net>
  * @copyright   2015 - 2020  Timothy J. Warren
  * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
- * @version     5.1
+ * @version     5.2
  * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
  */
 
@@ -263,6 +263,12 @@ final class Model {
 		]);
 
 		return $this->animeTransformer->transform($baseData);
+	}
+
+	public function getRandomLibraryAnime(string $status): Anime
+	{
+		// @TODO
+		return Anime::from([]);
 	}
 
 	/**
