1c721af0ba1db25cefce659b500e25a7c7d5ede4
Timothy J. Warren <tim@timshomepage.net>
2022-01-07 19:53:31 -0500

    Create image builder class to simplify creating placeholder images

---
+++ src/Ion/Exception/ImageCreationException.php
@@ -0,0 +1,26 @@
+<?php declare(strict_types=1);
+/**
+ * Hummingbird Anime List Client
+ *
+ * An API client for Kitsu to manage anime and manga watch lists
+ *
+ * PHP version 8
+ *
+ * @package     HummingbirdAnimeClient
+ * @author      Timothy J. Warren <tim@timshomepage.net>
+ * @copyright   2015 - 2021  Timothy J. Warren
+ * @license     http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version     5.2
+ * @link        https://git.timshomepage.net/timw4mail/HummingBirdAnimeClient
+ */
+
+namespace Aviat\Ion\Exception;
+
+use RuntimeException;
+
+/**
+ * Exception for bad configuration
+ */
+class ImageCreationException extends RuntimeException {
+	
+}
